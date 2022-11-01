a19ff070313c5a4d04c79f7175636ce3555485b0
sphade <sphade010@gmail.com>
2022-11-01 02:41:19 +0100

    added Base packages

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>African market</title>
+        <meta name="description" content="the african market web app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          African market
+          glpat-4Hp6_6VrcaALxNM_zXCF
+     </h1>
+      </main>
+
+     
+    </div>
+  )
+}
