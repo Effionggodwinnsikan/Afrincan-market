9edab7bbe5a4dd6d6f994be2dcf5a7cf24a7aac9
Adepejustephen <adepejuifeoluwa97@gmail.com>
2022-11-12 03:20:46 +0100

    add transition to login modal

--- components/modals/login/index.tsx
+++ components/modals/login/index.tsx
@@ -1,5 +1,6 @@
-import { Modal } from "@mui/material";
-import Link from "next/link";
+import { Fade, Modal } from "@mui/material";
+import Backdrop from "@mui/material/Backdrop";
+// import Link from "next/link";
 import React, { useState } from "react";
 import { TextInput } from "../../generalComponents/input";
 import ClearIcon from "@mui/icons-material/Clear";
@@ -11,55 +12,62 @@ interface LoginProps {
 }
 
 export const Login: React.FC<LoginProps> = ({ open, onClose, onClick }) => {
-
-  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
 
   function displayPassword() {
-    setShowPassword(!showPassword)
+    setShowPassword(!showPassword);
   }
-
 
   return (
     <>
-      <Modal open={open} onClose={onClose}>
-        <div className="flex flex-col border-0  rounded-xl p-4 absolute gap-6 left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] max-w-[31.25rem] w-full bg-white overflow-hidden shadow-modalContent md:rounded-2xl">
-          <div className="flex flex-col gap-4">
-            <div className="flex justify-end items-center relative h-[30px] w-full z-10 bg-white">
-              <button
-                className="flex items-center justify-center bg-[#E4E4E4] p-1 rounded-[50%] font-bold text-[32px] border-none outline-none cursor-pointer"
-                onClick={onClose}
-              >
-                <ClearIcon fontSize="inherit" />
-              </button>
-            </div>
-            <h2 className="text-2xl text-maintxt font-semibold">log in</h2>
-          </div>
-          <div className="flex flex-col gap-4">
-            <form action="" className="flex flex-col gap-4">
-              <TextInput label="Email " type="email" />
-              <div className="flex flex-col gap-2 items-left">
-                <TextInput
-                  label="Password "
-                  type={showPassword ? "text" : "password"}
-                />
+      <Modal
+        open={open}
+        onClose={onClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className="flex flex-col border-0  rounded-xl p-4 absolute gap-6 left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] max-w-[31.25rem] w-full bg-white overflow-hidden shadow-modalContent md:rounded-2xl">
+            <div className="flex flex-col gap-4">
+              <div className="flex justify-end items-center relative h-[30px] w-full z-10 bg-white">
                 <button
-                  className="border-none outline-none bg-none font-semibold text-xs w-[max-content]"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setShowPassword(!showPassword);
-                  }}
+                  className="flex items-center justify-center bg-[#E4E4E4] p-1 rounded-[50%] font-bold text-[32px] border-none outline-none cursor-pointer"
+                  onClick={onClose}
                 >
-                  {showPassword ? "Hide password" : "Show password"}
+                  <ClearIcon fontSize="inherit" />
                 </button>
               </div>
-              <button
-                className="bg-primaryBtn border-0 outline-0 text-white text-base font-semibold rounded-lg py-[0.625rem] px-2"
-                onClick={onClick}
-              >
-                Next
-              </button>
-            </form>
-            {/* <p className=" text-xs">
+              <h2 className="text-2xl text-maintxt font-semibold">Log in to your account</h2>
+            </div>
+            <div className="flex flex-col gap-4">
+              <form action="" className="flex flex-col gap-4">
+                <TextInput label="Email " type="email" />
+                <div className="flex flex-col gap-2 items-left">
+                  <TextInput
+                    label="Password "
+                    type={showPassword ? "text" : "password"}
+                  />
+                  <button
+                    className="border-none outline-none bg-none font-medium text-xs w-[max-content]"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setShowPassword(!showPassword);
+                    }}
+                  >
+                    {showPassword ? "Hide password" : "Show password"}
+                  </button>
+                </div>
+                <button
+                  className="bg-primaryBtn border-0 outline-0 text-white text-base font-semibold rounded-lg py-[0.625rem] px-2"
+                  onClick={onClick}
+                >
+                  Next
+                </button>
+              </form>
+              {/* <p className=" text-xs">
               Please visit
               <Link href="/">
                 <span className="text-primaryBtn font-semibold px-[2.5px]">
@@ -84,8 +92,9 @@ export const Login: React.FC<LoginProps> = ({ open, onClose, onClick }) => {
               </Link>
               apply.
             </p> */}
+            </div>
           </div>
-        </div>
+        </Fade>
       </Modal>
     </>
   );
