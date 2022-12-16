eb3280ce14520d4614735e09b4c76f127ca6d4be
Adepejustephen <adepejuifeoluwa97@gmail.com>
2022-12-16 13:06:54 +0100

    changed signup textfields to material ui

--- components/modals/signup/index.tsx
+++ components/modals/signup/index.tsx
@@ -1,12 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Fade, Modal } from "@mui/material";
 import Backdrop from "@mui/material/Backdrop";
 import ClearIcon from "@mui/icons-material/Clear";
-// import Link from "next/link";
-import { TextInput } from "../../generalComponents/input";
-// import { CheckBox, Select } from "../../generalComponents";
+
 import Link from "next/link";
 import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
 
 interface SignupProps {
   open: boolean;
@@ -17,8 +19,9 @@ interface SignupProps {
 export const Register: React.FC<SignupProps> = ({ open, onClose, onClick }) => {
   const [openOptions, setOpenOptions] = useState(false);
 
-  function handlefirstName() {
-    setOpenOptions(true)
+  function handlefirstName(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.value.length >= 1) setOpenOptions(true);
+    else if (e.target.value.length === 0) setOpenOptions(false);
   }
 
   return (
@@ -50,17 +53,103 @@ export const Register: React.FC<SignupProps> = ({ open, onClose, onClick }) => {
             <div className="flex flex-col gap-4">
               <form action="" className="flex flex-col gap-4">
                 <div className="flex flex-col md:flex-row gap-4 md:items-center">
-                  <TextInput
-                    label="First name "
-                    type="text"
-                    onChange={handlefirstName}
-                  />
-                  <TextInput label="Last name " type="text" />
+                  <FormControl
+                    sx={{ width: "100%", borderWidth: "2px" }}
+                    variant="outlined"
+                  >
+                    <InputLabel htmlFor="First-name">First name</InputLabel>
+                    <OutlinedInput
+                      id="First-name"
+                      onChange={handlefirstName}
+                      label="First name"
+                      sx={{
+                        paddingInlineStart: "2rem",
+                        backgroundColor: "#fff",
+                      }}
+                    />
+                  </FormControl>
+                  <FormControl
+                    sx={{ width: "100%", borderWidth: "2px" }}
+                    variant="outlined"
+                  >
+                    <InputLabel htmlFor="Last-name">Last name</InputLabel>
+                    <OutlinedInput
+                      id="Last-name"
+                      // onChange={handlefirstName}
+                      endAdornment={
+                        <InputAdornment position="start">
+                          {/* <FmdGoodOutlinedIcon className="absolute left-4 top-3.5 stroke-1 stroke-#202125 search-icon" /> */}
+                        </InputAdornment>
+                      }
+                      label="Last name"
+                      sx={{
+                        backgroundColor: "#fff",
+                      }}
+                    />
+                  </FormControl>
                 </div>
-                <TextInput label="Email " type="email" />
 
-                <TextInput label="password " type="password" />
-                <TextInput label="Verify password " type="password" />
+                <FormControl
+                  sx={{ width: "100%", borderWidth: "2px" }}
+                  variant="outlined"
+                >
+                  <InputLabel htmlFor="Email">Email</InputLabel>
+                  <OutlinedInput
+                    id="Email"
+                    type="email"
+                    // onChange={handlefirstName}
+
+                    label="Email"
+                    sx={{
+                      backgroundColor: "#fff",
+                    }}
+                  />
+                </FormControl>
+
+                {/* Passwords */}
+                <FormControl
+                  sx={{ width: "100%", borderWidth: "2px" }}
+                  variant="outlined"
+                >
+                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <OutlinedInput
+                    id="password"
+                    type="password"
+                    // onChange={handlefirstName}
+                    endAdornment={
+                      <InputAdornment position="start">
+                       
+                      </InputAdornment>
+                    }
+                    label="Password"
+                    sx={{
+                      backgroundColor: "#fff",
+                    }}
+                  />
+                </FormControl>
+                <FormControl
+                  sx={{ width: "100%", borderWidth: "2px" }}
+                  variant="outlined"
+                >
+                  <InputLabel htmlFor="veridy-password">
+                    Verify password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="verify-password"
+                    type="password"
+                    // onChange={handlefirstName}
+                    endAdornment={
+                      <InputAdornment position="start">
+                      
+                      </InputAdornment>
+                    }
+                    label="Verify password"
+                    sx={{
+                      backgroundColor: "#fff",
+                    }}
+                  />
+                </FormControl>
+               
                 {openOptions && (
                   <>
                     <div className="flex gap-4">
